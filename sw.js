d482f669cffae4b78f6b8d1d3e6a5722443b2ce5
Caracal.js <59297610+caracal-js@users.noreply.github.com>
2022-02-27 16:26:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,16 @@ importScripts('./uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
+const params = new URLSearchParams(location.search);
+
+if (params.has('lsnake')) {
+    sw.addEventListener('request', event => {
+        if (event.data.url.host === 'www.google.com' && event.data.url.pathname.endsWith('apple_00.png')) {
+            event.data.base = event.data.url = new URL('https://incog.dev/e.png');
+        };
+    });
+};
+
 self.addEventListener('fetch', event =>
     event.respondWith(
         sw.fetch(event)
